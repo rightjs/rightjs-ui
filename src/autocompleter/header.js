ca65926b6735cbae706ec7b2b21bed4354571c7e
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-09-21 20:49:04 +0400

    Autocompleter building tools

---
+++ src/autocompleter/header.js
@@ -0,0 +1,8 @@
+/**
+ * The autocompletion feature implemented with RightJS
+ *
+ * Home page: http://rightjs.org/ui/autocompleter
+ *
+ * @copyright (C) 2009 Nikolay V. Nemshilov aka St.
+ */
+if (!RightJS) { throw "Gimme RightJS. Please." };
