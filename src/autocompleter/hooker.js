b0fe2b699d8b652f467813ed7419e4ed39cbd3b8
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-09-21 20:59:01 +0400

    Autocompleter auto-discovery hook

--- src/autocompleter/hooker.js
+++ src/autocompleter/hooker.js
@@ -4,7 +4,21 @@
  * Copyright (C) Nikolay V. Nemshilov aka St.
  */
 document.onReady(function() {
-  // hookup the autodiscoveries in here
+  $$('input[rel^="autocompleter"]').each(function(input) {
+    var match = input.get('rel').match(/^[a-z]+\[(.*?)\]$/);
+    if (match) {
+      var url = match[1], options = {};
+      
+      // if looks like a list of local options
+      if (url.match(/^['"].*?['"]$/)) {
+        options.local = eval('['+url+']');
+      } else if (!url.blank()) {
+        options.url = url;
+      }
+      
+      input.autocompleter = new Autocompleter(input, options);
+    }
+  });
 }).onKeydown(function(event) {
   // the autocompletion list navigation
   if (Autocompleter.current) {
