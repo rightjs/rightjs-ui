03f0981c9465fa8e6021c7c97283fc37f69db0fa
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-09-27 11:13:07 +0400

    Sortable build tools

---
+++ src/sortable/header.js
@@ -0,0 +1,8 @@
+/**
+ * Sortable feature for RightJS (requires the Drag'n'Drop feature)
+ *
+ * See http://rightjs.org/ui/sortable
+ *
+ * Copyright (C) Nikolay V. Nemshilov aka St.
+ */
+if (!Draggable) throw "Gimme Draggable";
