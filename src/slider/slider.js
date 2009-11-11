263d970f77d919dfcec65d918f0e9d729198e6c8
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-11-11 22:28:04 +0300

    Slider basic functionality implementation

--- src/slider/slider.js
+++ src/slider/slider.js
@@ -8,10 +8,13 @@ var Slider = new Class(Observer, {
     EVENTS: $w('change'),
     
     Options: {
-      min:       0,         // the min value
-      max:       100,       // the max value
-      snap:      0,         // the values threshold
-      direction: 'auto'     // slider direction 'auto' or enforce with 'x', 'y'
+      min:       0,       // the min value
+      max:       100,     // the max value
+      snap:      0,       // the values threshold
+      value:     null,    // start value, if null then the min value will be used
+      direction: 'auto',  // slider direction 'auto' or enforce with 'x', 'y'
+      update:    null,    // reference to an element to update
+      round:     0        // the number of symbols after the decimal pointer
     },
     
     rescan: function() {
@@ -34,10 +37,13 @@ var Slider = new Class(Observer, {
    */
   initialize: function() {
     var args = $A(arguments);
-    this.element = (args[0] && !isHash(args[0])) ? $(args[0]) : this.build();
-    this.$super(isHash(args.last()) ? args.last() : eval('('+this.element.get('data-slider-options')+')'));
+    this.element = (args[0] && !isHash(args[0])) ? $(args.shift()) : this.build();
     
-    this.element._slider = this.init().reset();
+    this.$super(isHash(args[0]) ? args[0] : eval('('+this.element.get('data-slider-options')+')'));
+    
+    if (this.options.update) this.assignTo(this.options.update);
+    
+    this.element._slider = this.init();
   },
   
   // basic desctructor
@@ -56,8 +62,19 @@ var Slider = new Class(Observer, {
    * @return Slider this
    */
   setValue: function(value) {
-    this.value = isString(value) ? value.toFloat() : value;
-    return this.moveTo(this.value).fire('change', this.value);
+    var value = isString(value) ? value.toFloat() : value;
+    
+    if (this.options.snap) value = value - value % this.options.snap;
+    if (value < this.options.min) value = this.options.min;
+    if (value > this.options.max) value = this.options.max;
+    
+    this.moveTo(value);
+    
+    if (value !== this.value) {
+      this.fire('change', this.value = value);
+    }
+    
+    return this;
   },
   
   /**
@@ -70,12 +87,13 @@ var Slider = new Class(Observer, {
   },
   
   /**
-   * Sets the default value
+   * Sets the minimal value and rebuilds the dimensions cache
    *
+   * @param Number optional value to reset
    * @return Slider this
    */
-  reset: function() {
-    return this.moveTo(this.options.min);
+  reset: function(value) {
+    return this.precalc().setValue([value, this.options.value, this.options.min].compact()[0]);
   },
   
   /**
@@ -87,7 +105,7 @@ var Slider = new Class(Observer, {
    */
   insertTo: function(element, position) {
     this.element.insertTo(element, position);
-    return this.moveTo(this.value === null ? this.options.min : this.value);
+    return this.reset(this.value);
   },
   
   /**
@@ -97,14 +115,15 @@ var Slider = new Class(Observer, {
    * @return Slider this
    */
   assignTo: function(input) {
-    return this.onChange(function(value) {
+    return this.onChange(function(input, value) {
       var input = $(input);
-      input[input.setValue ? 'setValue' : 'update'](value);
-    });
+      input[input.setValue ? 'setValue' : 'update'](''+value);
+    }, input);
   },
   
 // protected
   
+  // inits the slider handle and resets the whole thing
   init: function() {
     this.handle = this.element.first('div.right-slider-handle')
       .makeDraggable({
@@ -112,45 +131,69 @@ var Slider = new Class(Observer, {
         onDrag: this.dragged.bind(this)
       });
       
-    return this;
+    if (this.options.direction == 'y')
+      this.element.addClass('right-slider-vertical');
+      
+    return this.reset();
   },
   
+  // builds the slider prorgrammatically
   build: function() {
     return $E('div', {'class': 'right-slider'}).insert($E('div', {'class': 'right-slider-handle'}));
   },
   
   // callback for the element dragg
-  dragged: function() {
-    //console.log('boo');
+  dragged: function(draggable, event) {
+    var position = draggable.element.style[this.direction == 'x' ? 'left' : 'top'].toFloat();
+    var value    = position / this.space * (this.options.max - this.options.min) + this.options.min;
+    
+    // rounding the value according to the options
+    var base = Math.pow(10, this.options.round);
+    value = (value * base).round() / base;
+    
+    this.setValue(value);
   },
   
   // callback for the draggable before event
   prepare: function(draggable) {
-    var options    = draggable.options;
-    var dimensions = this.element.dimensions();
-    var direction  = this.options.direction != 'auto' ? this.options.direction : 
-      dimensions.width > dimensions.height ? 'x' : 'y';
-    
     // moving the slider to the beginning position
-    this.moveTo(this.options.min);
-    var handle = this.handle.dimensions();
+    this.precalc().moveTo(this.value);
+    
+    var offset    = this.offset;
+    var element   = this.dimensions;
+    var options   = draggable.options;
     options.range = {};
     
     // calculating the ranges
-    if ((options.axis = direction) == 'x') {
-      options.range.x = [handle.left, handle.left + dimensions.width - (handle.left - dimensions.left) * 2];
+    if ((options.axis = this.direction) == 'x') {
+      options.range.x = [element.left + offset, element.left + element.width - offset];
     } else {
-      options.range.y = [handle.top, handle.top + dimensions.height - (handle.top - dimensions.top) * 2];
+      options.range.y = [element.top + offset, element.top  + element.height - offset];
     }
     
-    // moving the slider back to the current position
-    this.moveTo(this.value);
+    // calculating the snapping range
+    if (this.options.snap) {
+      options.snap = this.space / (this.options.max - this.options.min) * this.options.snap;
+    }
   },
   
   // moves the slider to the given position
   moveTo: function(value) {
+    this.handle.style[this.direction == 'x' ? 'left' : 'top'] = (
+      this.space / (this.options.max - this.options.min) * (value - this.options.min)
+    ) + 'px';
     
+    return this;
+  },
+  
+  // precalculates dimensions, direction and offset for further use
+  precalc: function() {
+    var handle      = this.handle.setStyle({left:'0', top:'0'}).dimensions();
     
+    this.dimensions = this.element.dimensions();
+    this.direction  = this.options.direction != 'auto' ? this.options.direction : this.dimensions.width > this.dimensions.height ? 'x' : 'y';
+    this.offset     = this.direction == 'x' ? handle.left - this.dimensions.left : handle.top - this.dimensions.top;
+    this.space      = (this.direction == 'x' ? this.dimensions.width - handle.width : this.dimensions.height - handle.height) - this.offset * 2;
     
     return this;
   }
