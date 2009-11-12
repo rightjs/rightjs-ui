c86d35fd64184d4d2cdea7f75f6dbdc67fde4551
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-11-12 18:43:27 +0300

    Slider build tools

---
+++ src/slider/header.js
@@ -0,0 +1,6 @@
+/**
+ * RightJS UI Slider unit (http://rightjs.org/ui/slider)
+ *
+ * Copyright (C) 2009 Nikolay V. Nemshilov aka St.
+ */
+if (!self.RightJS || !self.Draggable) throw "Gimme RightJS w/ DnD";
