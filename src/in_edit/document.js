c969aa8da24b377300e2b2c6f570d8d171cd7f42
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-12-28 20:21:02 +0300

    In place editor escape key handler fix

--- src/in_edit/document.js
+++ src/in_edit/document.js
@@ -3,7 +3,7 @@
  *
  * Copyright (C) Nikolay V. Nemshilov
  */
-document.onKeypress(function(event) {
+document.onKeydown(function(event) {
   if (event.keyCode == 27 && InEdit.current)
     InEdit.current.hide();
 });
