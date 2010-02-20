32f6ce7c63ec4bdd061a85ea5f7cca062e392609
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-02-20 12:47:40 +0300

    Uploader cleaning up and further implementation

--- src/uploader/uploader.js
+++ src/uploader/uploader.js
@@ -6,19 +6,18 @@
 var Uploader = new Class(Observer, {
   extend: {
     EVENTS: $w('start update finish error'),
-    
+
     Options: {
       url:         '/progress',
       param:       'X-Progress-ID',
-      
-      timeout:     400,
+
+      timeout:     1000,
       round:       0,
-      
-      formCssRule: '.with-progress',
-      barCssRule:  '.progress-bar'
+
+      formCssRule: '.with-progress'
     }
   },
-  
+
   /**
    * Basic constructor
    *
@@ -27,17 +26,17 @@ var Uploader = new Class(Observer, {
    */
   initialize: function(form, options) {
     this.form = $(form);
-    
+
     this.$super(options || eval('('+this.form.get('data-uploader-options')+')'));
-    
+
     this.element = this.find_or_build();
-    
+
     this.bar = this.element.first('.bar');
     this.num = this.element.first('.num');
-    
+
     this.form._uploader = this;
   },
-  
+
   /**
    * Starts the uploading monitoring
    *
@@ -45,19 +44,19 @@ var Uploader = new Class(Observer, {
    */
   start: function() {
     var data = {state: 'starting'};
-    return this.paint(data).prepare().fire('start', data).request();
+    return this.paint(data).prepare().request().fire('start', data);
   },
 
 // protected
-  
+
   // updates uploading bar progress
   update: function(data) {
     this.paint(data).fire('update', data);
-    
+
     switch (data.state) {
       case 'starting':
       case 'uploading':
-        this.request();
+        this.request.bind(this).delay(this.options.timeout);
         break;
       case 'done':
         this.fire('finish', data);
@@ -66,62 +65,65 @@ var Uploader = new Class(Observer, {
         this.fire('error', data);
         break;
     }
-    
+
     return this;
   },
 
   // changes the actual element styles
   paint: function(data) {
-    var percent = this.percent || 0;
-    
+    var percent = (this.percent || 0)/100;
+
     switch (data.state) {
       case 'starting':  percent = 0;   break;
-      case 'done':      percent = 100; break;
-      case 'uploading': percent = data.received / (data.size || 1);
+      case 'done':      percent = 1; break;
+      case 'uploading': percent = data.received / (data.size||1); break;
     }
 
-    this.percent = percent.round(this.options.round);
+    this.percent = (percent * 100).round(this.options.round);
+
     this.bar.style.width = this.percent + '%';
     this.num.innerHTML   = this.percent + '%';
-    
+
     // marking the failed uploads
-    this.element[data.state == 'error' ? 'addClass' : 'removeClass']('upload-failed');
-    
+    this.element[data.state == 'error' ? 'addClass' : 'removeClass']('right-progress-bar-failed');
+
     return this;
   },
-  
+
   // sends a request to the server
   request: function() {
-    (function() {
-      Xhr.load(this.options.url + "?" + this.options.param + "=" + this.uid, {
-        onSuccess: function(xhr) {
-          this.update(eval('('+xhr.json+')'));
-        }.bind(this)
-      });
-      
-    }).bind(this).delay(this.options.timeout);
-    
+    Xhr.load(this.options.url + "?" + this.options.param + "=" + this.uid, {
+      evalJSON: false,
+      onSuccess: function(xhr) {
+        this.update(eval('('+xhr.text+')'));
+      }.bind(this)
+    });
+
     return this;
   },
-  
+
   // prepares the form to carry the x-progress-id param
   prepare: function() {
-    this.uid = Math.random(1, 999999999999).toString(16);
+    this.uid = "";
+    for (i = 0; i < 32; i++) { this.uid += Math.random(0, 15).toString(16); }
+    
     var param = this.options.param;
     var url = this.form.action.replace(new RegExp('(\\?|&)'+RegExp.escape(param) + '=[^&]*', 'i'), '');
     this.form.action = (url.includes('?') ? '&' : '?') + param + '=' + this.uid;
+    
+    this.element.show();
     
     return this;
   },
 
   // finds or builds the progress-bar element
   find_or_build: function() {
-    var element = this.form.first(this.options.barCssRule) || $E('div').insertTo(this.form);
-    
+    var element = this.form.first('div.right-progress-bar') || $E('div').insertTo(this.form);
+
     if (element.innerHTML.blank())
       element.innerHTML = '<div class="bar"></div><div class="num"></div>';
-    
-    return element.addClass('progress-bar');
+
+    return element.addClass('right-progress-bar');
   }
-  
+
 });
