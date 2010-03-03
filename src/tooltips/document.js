0498f3d743557cd1404c38548c88ae837bca17ba
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-03-03 23:59:46 +0300

    Tooltips reworked to use events delegation

--- src/tooltips/document.js
+++ src/tooltips/document.js
@@ -1,14 +1,47 @@
 /**
  * The post load tooltips initialization script
  *
- * Copyright (C) 2009 Nikolay V. Nemshilov aka St.
+ * Copyright (C) 2009-2010 Nikolay V. Nemshilov aka St.
  */
 document.on({
-  ready: function() { Tooltip.rescan(); },
+  /**
+   * Watches all the mouse-over events and reacts if one of the targets
+   * matches a tooltip
+   *
+   * @param Event event
+   */
+  mouseover: function(event) {
+    var prev_tip = Tooltip.current, this_tip = Tooltip.find(event);
+    
+    if (this_tip) {
+      if (prev_tip && prev_tip !== this_tip) prev_tip.hide();
+      if (this_tip.element.hidden()) this_tip.showDelayed();
+      
+      this_tip.moveTo(event);
+    }
+  },
   
+  /**
+   * Catches the mouseout events and hides tooltips when needed
+   *
+   * @param Event event
+   */
+  mouseout: function(event) {
+    var curr_tip = Tooltip.current, this_tip = Tooltip.find(event);
+    
+    if (curr_tip && (!this_tip || this_tip === curr_tip))
+      curr_tip.hide();
+  },
+  
+  /**
+   * Moves tooltips when active
+   *
+   * @param Event event
+   */
   mousemove: function(event) {
-    if (Tooltip.current) {
-      Tooltip.current.moveTo(event);
+    var tip = Tooltip.current;
+    if (tip && tip.options.move) {
+      tip.moveTo(event);
     }
   }
 });
--- src/tooltips/tooltip.js
+++ src/tooltips/tooltip.js
@@ -1,8 +1,7 @@
 /**
  * The native tooltips feature for RithJS
  *
- *
- * Copyright (C) 2009 Nikolay V. Nemshilov aka St.
+ * Copyright (C) 2009-2010 Nikolay V. Nemshilov aka St.
  */
 
 var Tooltip = new Class({
@@ -10,31 +9,44 @@ var Tooltip = new Class({
   
   extend: {
     Options: {
-      checkTags:  '*', // tags that should be checked on-load
+      cssRule:    '[rel=tooltip]',  // a css-marker of an element with a tooltip
+      
+      fxName:     'fade',           // the appearance effect name
+      fxDuration: 400,              // the appearance effect duration
+      delay:      400,              // the appearance delay
+      
+      move:       true,             // if it should be moved with the mouse
+      
+      idSuffix:   '-tooltip',       // ID prefix for tooltips with ID
+      
+      // deprecated options
       relName:    'tooltip',
-      idSuffix:   '-tooltip',
-      fxName:     'fade',
-      fxDuration: 400,
-      delay:      400  // the appearance delay
+      checkTags:  '*'
     },
     
     current: null, // currently active tooltip reference
+    instances: [], // keeps the list of instances
     
-    // scans the page for auto-discoverable tooltiped elements
-    rescan: function(scope) {
-      var key = Tooltip.Options.relName;
-      ($(scope) || document).select(Tooltip.Options.checkTags+'[rel='+key+']').each(function(element) {
-        var text = element.get('title') || element.get('alt');
-        if (text) {
-          var data = element.get('data-'+key+'-options');
-          new Tooltip(element, eval('('+data+')'));
-        }
-      });
-    }
+    // tries to find a tip closest to the event
+    find: function(event) {
+      var target  = event.target, targets = [target].concat(target.parents()),
+          targets = targets.slice(0, targets.length - 2),
+          element = targets.first(function(node) {
+            return node._tooltip || node.match(Tooltip.Options.cssRule);
+          });
+      
+      if (element) {
+        var uid = $uid(element);
+        return Tooltip.instances[uid] = Tooltip.instances[uid] || new Tooltip(element);
+      }
+    },
+    
+    // DEPRECATED a dummy method for the old API backward compatibility
+    rescan: function(scope) { }
   },
   
   initialize: function(element, options) {
-    this.element   = $E('div', {'class': 'right-tooltip'}).insertTo(document.body).hide();
+    this.element   = $E('div', {'class': 'right-tooltip'}).hide().insertTo(document.body);
     this.container = $E('div', {'class': 'right-tooltip-container'}).insertTo(this.element);
     
     this.setOptions(options).assignTo(element);
@@ -50,30 +62,33 @@ var Tooltip = new Class({
   },
   
   hide: function() {
-    Tooltip.current = null;
     this.cancelTimer();
-    this.element.hide(this.options.fxName, {duration: this.options.fxDuration});
+    this.element.hide(this.options.fxName, {
+      duration: this.options.fxDuration,
+      onFinish: function() {
+        if (Tooltip.current === this)
+          Tooltip.current = null;
+      }.bind(this)
+    });
     
     return this;
   },
   
   show: function() {
-    Tooltip.current = this;
-    this.element.show(this.options.fxName, {duration: this.options.fxDuration});
+    this.element.stop().show(this.options.fxName, {duration: this.options.fxDuration});
 
-    return this;
+    return Tooltip.current = this;
   },
   
   showDelayed: function() {
+    // hidding all the others
+    Tooltip.instances.each(function(tip) {
+      if (tip && tip !== this) tip.hide();
+    }, this);
+    
     Tooltip.current = this;
+    
     this.timer = this.show.bind(this).delay(this.options.delay);
-  },
-  
-  cancelTimer: function() {
-    if (this.timer) {
-      this.timer.cancel(); 
-      this.timer = null;
-    }
   },
   
   moveTo: function(event) {
@@ -83,17 +98,20 @@ var Tooltip = new Class({
     return this;
   },
   
+// protected
+  
+  cancelTimer: function() {
+    if (this.timer) {
+      this.timer.cancel(); 
+      this.timer = null;
+    }
+  },
+  
   assignTo: function(element) {
     this.setText(element.get('title') || element.get('alt'));
     
     // removing the element native title and alt
-    element.setAttribute('title', '');
-    element.setAttribute('alt', '');
-    
-    element.on({
-      mouseover: this.showDelayed.bind(this),
-      mouseout:  this.hide.bind(this)
-    });
+    element.set({ title: '', alt: ''});
     
     if (element.id) this.element.id = element.id + this.options.idSuffix;
     
