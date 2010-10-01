80fcdf79a537bc2a9ee776868bbe5a3f019fe61d
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-10-01 17:55:01 +0400

    Italian i18n module

---
+++ i18n/it.js
@@ -0,0 +1,42 @@
+/**
+ * RightJS UI Internationalization: Italian module
+ *
+ * Copyright (C) Nikolay Nemshilov
+ */
+if (self.Calendar) {
+  $ext(Calendar.i18n, {
+    Done:           'Fatto',
+    Now:            'Oggi',
+    NextMonth:      'Mese successivo',
+    PrevMonth:      'Mese precedente',
+    NextYear:       'Anno seguente',
+    PrevYear:       'Anno precedente',
+
+    dayNames:        $w('Domenica Lunedi Martedi Mercoledi Giovedi Venerdi Sabato'),
+    dayNamesShort:   $w('Dom Lun Mar Mer Gio Ven Sab'),
+    dayNamesMin:     $w('Do Lu Ma Me Gi Ve Sa'),
+    monthNames:      $w('Gennaio Febbraio Marzo Aprile Maggio Giugno Luglio Agosto Settembre Ottobre Novembre Dicembre'),
+    monthNamesShort: $w('Gen Feb Mar Apr Mag Giu Lug Ago Set Ott Nov Dic')
+  });
+}
+
+if (self.Lightbox) {
+  $ext(Lightbox.i18n, {
+    Close: 'Chiudi',
+    Prev:  'Immagine precedente',
+    Next:  'Immagine seguente'
+  });
+}
+
+if (self.InEdit) {
+  $ext(InEdit.i18n, {
+    Save:   "Salva",
+    Cancel: "Abbandona"
+  });
+}
+
+if (self.Colorpicker) {
+  $ext(Colorpicker.i18n, {
+    Done: 'Fatto'
+  });
+}
