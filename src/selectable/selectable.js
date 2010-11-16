c2e712cb000503d28948d9b4b31d5be47d838440
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-11-16 16:25:11 +0300

    Selectable empty option issue fix

--- src/selectable/selectable.js
+++ src/selectable/selectable.js
@@ -6,27 +6,27 @@
 var Selectable = new Class(Observer, {
   extend: {
     EVENTS: $w('change select unselect disable enable hover leave show hide'),
-    
+
     Options: {
       options:    null,    // a hash of key-value pairs
       selected:   null,    // an array of selected keys
       disabled:   null,    // an array of disabled keys
-      
+
       multiple:   true,    // a flag if it shoulde a multiselect or a single select widget
-      
+
       fxName:     'slide', // the drop-down options list fx-name null, 'slide', 'fade'
       fxDuration: 'short', // the drop-down options list fx-duration
-      
+
       update:     null,    // a field to be assigned to
       parseIds:   false,   // if it should parse integer ids out of the keys
-      
+
       limit:      null,    // put some number if you'd like to limit the number of selected items
-      
+
       hCont  :   '&bull;', // single-selectable handle content
-      
+
       refresh:    true     // a flag if it should automatically refresh the items list
     },
-    
+
     rescan: function(scope) {
       ($(scope)||document).select('*.right-selectable').each(function(element) {
         if (!element._selectable) {
@@ -35,14 +35,14 @@ var Selectable = new Class(Observer, {
       });
     }
   },
-  
+
   // css-classes
   baseClass:      'right-selectable',
   singleClass:    'right-selectable-single',
   selectedClass:  'right-selectable-selected',
   disabledClass:  'right-selectable-disabled',
   containerClass: 'right-selectable-container',
-  
+
   /**
    * Basic constructor
    *
@@ -51,24 +51,24 @@ var Selectable = new Class(Observer, {
    */
   initialize: function() {
     var args = $A(arguments), selectbox;
-    
+
     if (args[0] && !isHash(args[0])) this.element = $(args[0]);
     this.$super(isHash(args.last()) ? args.last() : this.element ?
       eval('('+this.element.get('data-selectable-options')+')') : null);
-    
+
     if (!this.element)
       this.element = this.build();
     else if ((selectbox = this.element.tagName == 'SELECT')) {
       this.selectbox = this.harvestOptions(this.element);
       this.element   = this.build().insertTo(this.selectbox, 'before');
     }
-    
+
     this.element._selectable = this.init();
-    
+
     if (selectbox)
       this.assignTo(this.hideOriginal(this.selectbox));
   },
-  
+
   // standard descructor
   destroy: function() {
     this.items.each(function(item) {
@@ -78,10 +78,10 @@ var Selectable = new Class(Observer, {
         .stopObserving('mousedown', this.onMousedown)
         .stopObserving('mouseup',   this.onMouseup);
     }, this);
-    
+
     delete(this.element._selectable);
   },
-  
+
   /**
    * Sets the value
    *
@@ -91,13 +91,13 @@ var Selectable = new Class(Observer, {
   setValue: function(value) {
     // parsing the value
     if (isString(value)) value = value.split(',').map('trim').filter(function(s) { return !s.blank(); });
-    
+
     // resetting the selections
     this.items.each('removeClass', this.selectedClass);
-    
+
     return this.select(value);
   },
-  
+
   /**
    * Returns the list of selected items
    *
@@ -113,7 +113,7 @@ var Selectable = new Class(Observer, {
       }, this);
     }
   },
-  
+
   /**
    * Inserts the widget into the element at the given position
    *
@@ -125,7 +125,7 @@ var Selectable = new Class(Observer, {
     this[this.isSingle ? 'container' : 'element'].insertTo(element, position);
     return this;
   },
-  
+
   /**
    * Assigns the widget to work in pair with the input element
    *
@@ -135,11 +135,19 @@ var Selectable = new Class(Observer, {
   assignTo: function(element) {
     var assign  = function(element, value) {
       if (element = $(element)) {
-        if (value === undefined || value === null) value = '';
+        if (value === undefined || value === null) { value = ''; }
+
         element[element.setValue ? 'setValue' : 'update'](element.type == 'select-multiple' ? value : ''+value);
+
+        // fixing FF fuckup with 'selected' option set 'true'
+        if (element.type == 'select-one') {
+          $A(element.getElementsByTagName('OPTION')).each(function(option) {
+            option.selected = option.value == value;
+          });
+        }
       }
     }.curry(element);
-    
+
     var connect = function(element, object) {
       var element = $(element);
       if (element && element.onChange) {
@@ -148,7 +156,7 @@ var Selectable = new Class(Observer, {
         }.bind(object));
       }
     }.curry(element);
-    
+
     if ($(element)) {
       assign(this.getValue());
       connect(this);
@@ -158,10 +166,10 @@ var Selectable = new Class(Observer, {
         connect(this);
       }.bind(this));
     }
-    
+
     return this.onChange(assign);
   },
-  
+
   /**
    * disables the given key or keys
    * NOTE: if no keys specified, then all the items will be disabled
@@ -175,7 +183,7 @@ var Selectable = new Class(Observer, {
     }, this);
     return this;
   },
-  
+
   /**
    * disables the given key or keys
    * NOTE: if no keys specified, then all the items will be enabled
@@ -189,7 +197,7 @@ var Selectable = new Class(Observer, {
     }, this);
     return this;
   },
-  
+
   /**
    * Checks if the given key or keys are disabled
    * NOTE: if no keys specified, then will check if all the items are disabled
@@ -200,7 +208,7 @@ var Selectable = new Class(Observer, {
   disabled: function(keys) {
     return this.mapOrAll(keys).every('hasClass', this.disabledClass);
   },
-  
+
   /**
    * selects item(s) that refers to the given key or keys
    *
@@ -209,12 +217,12 @@ var Selectable = new Class(Observer, {
    */
   select: function(keys) {
     var items = this.mapEnabled(keys), selected_class = this.selectedClass;
-    
+
     if (this.isSingle && items) {
       this.items.each('removeClass', selected_class);
       items = [items[0]];
     }
-    
+
     // applying the selection limit if ncessary
     if (!this.isSingle && this.options.limit) {
       var selected = this.items.filter('hasClass', selected_class), clean = [];
@@ -225,14 +233,14 @@ var Selectable = new Class(Observer, {
       }
       items = clean;
     }
-    
+
     items.compact().each(function(item) {
       this.fire('select', item.addClass(selected_class));
     }, this);
-    
+
     return this;
   },
-  
+
   /**
    * Unselects item(s) that refers to the given key or keys
    *
@@ -245,7 +253,7 @@ var Selectable = new Class(Observer, {
     }, this);
     return this;
   },
-  
+
   /**
    * Checks if item(s) are selected
    *
@@ -255,7 +263,7 @@ var Selectable = new Class(Observer, {
   selected: function(keys) {
     return this.mapEnabled(keys).every('hasClass', this.selectedClass);
   },
-  
+
   /**
    * Checks for new selectees in the element and updates the internal index
    *
@@ -273,29 +281,29 @@ var Selectable = new Class(Observer, {
         });
       }
     }, this);
-    
+
     return this;
   },
-  
+
 // protected
 
   // initis the widget
   init: function() {
     this.element.addClass(this.baseClass);
-    
+
     if (this.isSingle = !this.options.multiple || this.element.hasClass(this.singleClass)) {
       this.buildSingle().element.addClass(this.singleClass);
     }
-    
+
     // creating static callbacks so they could be detached
     this.onMousedown = this.mousedown.bind(this);
     this.onMouseup   = this.mouseup.bind(this);
     this.onMouseover = this.mouseover.bind(this);
     this.onMouseout  = this.mouseout.bind(this);
     this.onClick     = this.click.bind(this);
-    
+
     this.value = null;
-    
+
     var on_change = function() {
       var value = this.getValue();
       if (value != this.value) {
@@ -303,9 +311,9 @@ var Selectable = new Class(Observer, {
         this.fire('change', value, this);
       }
     }.bind(this);
-    
+
     this.refresh().onSelect(on_change).onUnselect(on_change);
-    
+
     if (this.isSingle)         this.onSelect('showItem');
     if (this.options.disabled) this.disable(this.options.disabled);
     if (this.options.selected) this.select(this.options.selected);
@@ -314,7 +322,7 @@ var Selectable = new Class(Observer, {
       if (this.options.selected === null) this.select(this.items[0]);
       this.onSelect('hideList'); // don't move it upper, it will force the hide effect
     }
-    
+
     // auto-refresh feature
     if (this.options.refresh) {
       var old_insert = this.element.update;
@@ -324,16 +332,16 @@ var Selectable = new Class(Observer, {
         return result;
       }.bind(this);
     }
-    
+
     return this;
   },
-  
+
   // finds out the value for the item
   itemValue: function(item) {
     var value = item.id || item.val;
     return value ? this.options.parseIds ? value.match(/\d+/) : value : this.items.indexOf(item);
   },
-  
+
   // wrapping the events trigger to feed it with some more options
   fire: function(name, item) {
     if (item && item.tagName) {
@@ -343,41 +351,41 @@ var Selectable = new Class(Observer, {
     }
     return this;
   },
-  
+
   // collects the item elements by the various key defs
   map: function(keys) {
     if (!isArray(keys)) keys = [keys];
-    
+
     return keys.map(function(key) {
       var index = (isString(key) && /^\d+$/.test(key)) ? key.toInt() : key, item = key;
-      
+
       if (isNumber(index)) {
         item = this.items[index];
       } else if(isString(key)) {
         item = this.items.first(function(i) { return i.id == key || i.val == key; });
       }
-      
+
       return item;
     }, this).compact();
   },
-  
+
   // returns matching items or all of them if there's no key
   mapOrAll: function(keys) {
     return defined(keys) ? this.map(keys) : this.items;
   },
-  
+
   // maps and filters only enabled items
   mapEnabled: function(keys) {
     return this.mapOrAll(keys).filter(function(item) {
       return !item.hasClass(this.disabledClass);
     }, this);
   },
-  
+
   // onmousedown callback
   mousedown: function(event) {
     event.stop();
     var item = event.target;
-    
+
     if (!this.disabled(item)) {
       if (this.isSingle) {  // single-selects are always select
         this.select(item);
@@ -388,40 +396,40 @@ var Selectable = new Class(Observer, {
         this.select(item);
         this._massSelect = true; // mass-selection start
       }
-      
+
       // mass-selection with a shift/meta key
       if ((event.shiftKey || event.metaKey) && this._prevItem) {
         var index1 = this.items.indexOf(this._prevItem);
         var index2 = this.items.indexOf(item);
-        
+
         if (index1 != index2) {
           if (index1 > index2) {
             var t = index1;
             index1 = index2;
             index2 = index1;
           }
-            
+
           for (var i=index1; i < index2; i++) {
             this[this._prevItem.hasClass(this.selectedClass) ? 'select' : 'unselect'](this.items[i]);
           }
         }
       }
-      
+
       this._prevItem = item;
     }
   },
-  
+
   // onmouseup callback
   mouseup: function(event) {
     event.stop();
     this._massRemove = this._massSelect = false; // mass-selection stop
   },
-  
+
   // mouseover callback
   mouseover: function(event) {
     var item = event.target;
     this.fire('hover', item);
-    
+
     if (!this.isSingle) {
       if (this._massSelect) {
         this.select(item);
@@ -430,21 +438,21 @@ var Selectable = new Class(Observer, {
       }
     }
   },
-  
+
   // mouseout callback
   mouseout: function(event) {
     this.fire('leave', event.target);
   },
-  
+
   // mouseclick callback
   click: function(event) {
     event.stop();
   },
-  
+
   // builds the widget programmatically
   build: function() {
     var element = $E('ul'), options = this.options.options, items = [];
-    
+
     if (isArray(options)) {
       options.each(function(option) {
         items.push(isArray(option) ? option : [option, option]);
@@ -458,10 +466,10 @@ var Selectable = new Class(Observer, {
     items.each(function(option) {
       element.insert($E('li', {val: option[1], html: option[0]}));
     });
-    
+
     return element;
   },
-  
+
   // builds a container for a single-select
   buildSingle: function() {
     this.container = $E('div', {'class': this.containerClass})
@@ -470,32 +478,32 @@ var Selectable = new Class(Observer, {
         $E('ul', {'class': 'right-selectable-display'})
       ])
       .onClick(this.toggleList.bind(this));
-    
+
     if (this.element.parentNode) {
       this.container.insertTo(this.element, 'instead');
     }
-    
+
     this.container.insert(this.element);
-      
+
     document.onClick(this.hideList.bind(this));
-    
+
     return this;
   },
-  
+
   // toggles the single-selects list
   toggleList: function(event) {
     event.stop();
     return this.element.getStyle('display') !== 'none' ? this.hideList() : this.showList(event);
   },
-  
+
   // shows list for the single-selects
   showList: function(event) {
     event.stop();
     if (this.isSingle) {
       $$('.right-selectable-single').without(this.element).each('hide');
-      
+
       var dims = this.container.dimensions(), pos = this.container.position();
-      
+
       this.element.setStyle({
         top: (dims.top + dims.height - pos.y) + 'px',
         left: (dims.left - pos.x) + 'px',
@@ -504,11 +512,11 @@ var Selectable = new Class(Observer, {
         duration: this.options.fxDuration,
         onFinish: this.fire.bind(this, 'show', this)
       });
-      
+
       if (!this.options.fxName) this.fire('show', this);
     }
   },
-  
+
   // hides the list for the single-selects
   hideList: function() {
     if (this.isSingle && this.element.getStyle('display') !== 'none') {
@@ -516,16 +524,16 @@ var Selectable = new Class(Observer, {
         duration: this.options.fxDuration,
         onFinish: this.fire.bind(this, 'hide', this)
       });
-      
+
       if (!this.options.fxName) this.fire('hide', this);
     }
   },
-  
+
   // shows the item in the main view of a single-selector
   showItem: function(item) {
     this.container.first('ul').update(item ? $E('li', {html: item.innerHTML}) : '<li>&nbsp;</li>');
   },
-  
+
   // harvests options from a selectbox element
   harvestOptions: function(box) {
     var options = this.options;
@@ -534,19 +542,19 @@ var Selectable = new Class(Observer, {
       options.options  = [];
       options.selected = [];
       options.disabled = [];
-      
+
       $A(box.getElementsByTagName('OPTION')).each(function(option, index) {
         options.options.push([option.innerHTML, $(option).get('value') || option.innerHTML]);
-        
+
         if (option.selected && !box.disabled) options.selected.push(index);
         if (option.disabled ||  box.disabled) options.disabled.push(index);
       });
-      
+
       if (options.selected.empty()) options.selected = 0;
     }
     return box;
   },
-  
+
   hideOriginal: function(element) {
     return element.wrap($E('div', {
       style: 'position:absolute;z-index:-1;visibility:hidden;width:0;height:0;overflow:hidden'
